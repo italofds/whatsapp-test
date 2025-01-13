5314b763417e01801e4c88f5d6030741ca77e9a2
italofds <italobragasantos@hotmail.com>
2025-01-13 11:30:38 -0300

    chromium path

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
   // Adicione abaixo as opções de puppeteer
   puppeteer: {
     headless: true,
-    executablePath: '/usr/bin/chromium-browser', 
+    executablePath: '/usr/bin/chromium', 
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox'
