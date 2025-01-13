4bd1fff67ad0740327a19044bfd28235ab9c1613
italofds <italobragasantos@hotmail.com>
2025-01-13 11:34:51 -0300

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 const client = new Client({
   authStrategy: new LocalAuth(),
